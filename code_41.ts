612d84446993ff34092f6542196b911d80054480
Disavito <dsvt98@gmail.com>
2025-09-17 11:05:19 -0500

    Delete code_41.ts

--- code_41.ts
+++
@@ -1,5 +0,0 @@
-if (values.transactionType === 'Devolucion' || values.transactionType === 'Gasto') {
-        finalAmount = -Math.abs(values.amount); // Ensure it's negative
-      } else if (values.transactionType === 'Anulacion') {
-        finalAmount = 0;
-      }
