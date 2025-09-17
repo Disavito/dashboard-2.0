ac1e3ced43c93c6a57c9a52f5f5d434952c82ffd
Disavito <dsvt98@gmail.com>
2025-09-17 10:58:45 -0500

    Delete code_1.sh

--- code_1.sh
+++
@@ -1,8 +0,0 @@
-npm run build
-> vite-shadcn@0.0.0 build
-> tsc -b && vite build
-src/pages/People.tsx:19:10 - error TS6133: 'SocioTitular' is declared but its value is never read.
-19 import { SocioTitular, SocioTitularWithReceipt } from '@/lib/types'; // Import both SocioTitular and SocioTitularWithReceipt
-~~~~~~~~~~~~
-Found 1 error.
-\
